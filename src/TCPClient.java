fba6e36c82bd1239182a513e3a30551de3afcdb4
alopex <arnout.devos@student.kuleuven.be>
2014-03-03 12:48:12 +0100

    Client get requests work with stackoverflow.com

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -4,23 +4,46 @@ class TCPClient
 {
 	public static void main(String argv[]) throws Exception
 	{
-		BufferedReader inFromUser = new BufferedReader( new
-				InputStreamReader(System.in));
 		while(true){
-			Socket clientSocket = new Socket("localhost", 6789);
+//			BufferedReader inFromUser = new BufferedReader( new
+//					InputStreamReader(System.in));
+			Socket clientSocket = new Socket(InetAddress.getByName("stackoverflow.com"), 80);
 			DataOutputStream outToServer = new DataOutputStream
 					(clientSocket.getOutputStream());
 			BufferedReader inFromServer = new BufferedReader(new
 					InputStreamReader(clientSocket.getInputStream()));
-
-			String sentence = inFromUser.readLine();
-			outToServer.writeBytes(sentence + '\n');
-			String modifiedSentence = inFromServer.readLine();
-			System.out.println("FROM SERVER: " + modifiedSentence);
+//			String inputLine;
+//	        StringBuilder request = new StringBuilder();
+//				while((inputLine = inFromUser.readLine()) != null){
+//					request.append(inputLine);
+//				}
+//				System.out.println("REQUEST WAS: " + request.toString());
+//				
+//			if(request.toString().toUpperCase().startsWith("EXIT")){
+//				clientSocket.close();
+//				break;
+//			}
+			
+			//String sentence = inFromUser.readLine();
+			outToServer.writeBytes("GET / HTTP/1.1");
+			outToServer.writeBytes("\r\n");
+			outToServer.writeBytes("Host: stackoverflow.com");
+			outToServer.writeBytes("\r\n");
+			outToServer.writeBytes("\r\n");
+			outToServer.flush();
+			
+			String t;
+			while((t = inFromServer.readLine()) != null) System.out.println(t);
+			inFromServer.close();
 			clientSocket.close();
-			if(sentence.equals("exit"))
-				break;
+			//String modifiedSentence = inFromServer.readLine();
+			
+//			inputLine = null;
+//	        StringBuilder response = new StringBuilder();
+//				//while((inputLine = inFromServer.readLine()) != null){
+//	        		response.append(inFromServer.readLine());//response.append(inputLine);
+//				//}
+//			System.out.println(response);
 		}
-		
 	}
 }
