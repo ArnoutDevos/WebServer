7f1e2d4ae761939efff9fc276133414a2494fa98
jakob <jakob.dhondt@student.kuleuven.be>
2014-03-05 20:08:55 +0100

    faslkfj

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -6,7 +6,7 @@ class TCPClient
 	{
 		BufferedReader inFromUser = new BufferedReader( new
 				InputStreamReader(System.in));
-		Socket clientSocket = new Socket(InetAddress.getByName("www.example.com"), 80);
+		Socket clientSocket = new Socket(InetAddress.getByName("localhost"), 6789);
 		DataOutputStream outToServer = new DataOutputStream
 				(clientSocket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader(new
@@ -29,7 +29,6 @@ class TCPClient
 			
 			String request = inFromUser.readLine();
 			System.out.println("REQUEST WAS: " + request);
-			
 			outToServer.writeBytes(request + "\r\n");
 			outToServer.flush();
 //			if(inFromServer.ready()){
