bf69f29eb23b1a1fc757f4f2f10c2f237cf2ac96
alopex <arnout.devos@student.kuleuven.be>
2014-03-06 01:14:35 +0100

    Backup with comments ^^

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -28,10 +28,12 @@ class TCPClient
 				System.out.println("REQUEST WAS: " + t);
 			}
 			t = "";
+			System.out.println("Retrieving file.");
 			while(inFromServer.ready()){
 				t += inFromServer.readLine() + "\n";
 			}
 			System.out.println(t);
+			clientSocket.close();
 			Document doc = Jsoup.parse(t);
 			Elements media = doc.select("[src]");
 			System.out.println("Retrieving embedded elements.");
@@ -46,22 +48,120 @@ class TCPClient
 						(clientSocketEmbedded.getOutputStream());
 				BufferedReader inFromServerEmbedded = new BufferedReader(new
 						InputStreamReader(clientSocketEmbedded.getInputStream()));
-				System.out.println("GET request: " + "GET " + src.attr("src") + " HTTP 1.1\r\n");
-				outToServerEmbedded.writeBytes("GET " + src.attr("src") + " HTTP 1.1\r\n");
-				outToServerEmbedded.writeBytes("Host: " + temp + " HTTP 1.1\r\n");
-				outToServerEmbedded.writeBytes("\r\n");
+//				File mediaFile = new File("C:\\");
+//				FileOutputStream fileStream = new FileOutputStream(mediaFile);
+				String filename = extractFileName(url.getPath());
+				File file = new File("C:\\Client\\"+filename);
+//				if (!file.exists()) {
+//					file.createNewFile();
+//				}
+				String request = "GET " + url.getPath() + " HTTP/1.1\r\n"+"Host: " + temp + "\r\n\r\n";
+				System.out.println("Request: "+request);
+				outToServerEmbedded.writeBytes(request);
 				outToServer.flush();
-				String result = "";
-				while(!inFromServerEmbedded.ready())
-				while(!((temp = inFromServerEmbedded.readLine()) == null) && !(temp.isEmpty())){
-					result += temp + "\n";
+				
+				//FileWriter fw = new FileWriter(file.getAbsoluteFile());
+				//BufferedWriter bw = new BufferedWriter(fw);
+		        
+				
+				InputStream inputStream = null;
+				OutputStream outputStream = null;
+				
+				//String result = "";
+				//Boolean realData = false;
+//				int length = 0;
+//				while(!inFromServerEmbedded.ready())
+//				while(!((temp = inFromServerEmbedded.readLine()) == null) || inFromServerEmbedded.ready()){//!((temp = inFromServerEmbedded.readLine()) == null) && !(temp.isEmpty()) ||
+//					result += temp;
+//					if(temp.toLowerCase().startsWith("content-length: ")){
+//						length = Integer.parseInt(temp.toLowerCase().replace("content-length: ",""));
+//					}
+//					if(temp.equals("")) break;
+//				}
+				
+				inputStream = clientSocketEmbedded.getInputStream();
+				 
+				// write the inputStream to a FileOutputStream
+				outputStream = new FileOutputStream(file.getAbsoluteFile());
+		 
+				int read = 0;
+				byte[] bytes = new byte[30000];
+		 
+				while ((read = inputStream.read(bytes)) != -1) {
+					outputStream.write(bytes, 0, read);
 				}
-				System.out.println(result);
+		 
+				System.out.println("Done!");
+				
+				//Boolean realData = false;
+//				int length = 0;
+//				while(!inFromServerEmbedded.ready())
+//				while(!((temp = inFromServerEmbedded.readLine()) == null) || inFromServerEmbedded.ready()){//!((temp = inFromServerEmbedded.readLine()) == null) && !(temp.isEmpty()) ||
+//					//temp = inFromServerEmbedded.readLine();
+//					result += temp;
+//					if(temp.toLowerCase().startsWith("content-length: ")){
+//						length = Integer.parseInt(temp.toLowerCase().replace("content-length: ",""));
+//					}
+//					if(temp.equals("")) break;
+//				}
+//				byte[] dummy = temp.getBytes();
+//				int offset = dummy.length;
+//				int header = dummy.length;
+				
+				
+				//BufferedInputStream in = new BufferedInputStream(clientSocketEmbedded.getInputStream());
+				//InputStream in = clientSocketEmbedded.getInputStream();
+	            //FileOutputStream fout = new FileOutputStream(file.getAbsolutePath());
+
+//	            byte[] bytes = new byte[length];
+//	            dummy = new byte[length];
+//			      int numRead = 0;
+//			      while (offset < bytes.length && !((numRead=in.read(dummy, 0, 10)) < 0)){
+//			          offset += numRead;
+//			      }
+//			      
+//			      if (offset < bytes.length) {
+//			          throw new IOException("Could not completely read file " + file.getName() + ", offset = " + offset + ", bytes.length = " + bytes.length+", header length = "+header);
+//			      }
+			      
+//	            byte data[] = new byte[1024];
+//	            int count;
+//	            while ((count = in.read(data, 0, 1024)) != -1)
+//	            {
+//	                fout.write(data, 0, count);
+//	            }
+//	            fout.close();
+				
+				
+				//bos.flush();
+				 //bw.close();
+				 //System.out.println(result);
+				System.out.println("File "+filename+" written: "+file.exists());
 				clientSocketEmbedded.close();
 	        }
 			
-			clientSocket.close();
+			
 			}
 		}
+	 private static String extractFileName(String path) {
+
+		    if ( path == null)
+		    {
+		      return null;
+		    }
+		    String newpath = path.replace('\\','/');
+		    int start = newpath.lastIndexOf("/");
+		    if ( start == -1)
+		    {
+		      start = 0;
+		    }
+		    else
+		    {
+		      start = start + 1;
+		    }
+		    String pageName = newpath.substring(start, newpath.length());
+
+		    return pageName;
+		  }
 		
 	}
