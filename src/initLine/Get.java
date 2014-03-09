0a6bad0c91b03522bcdc3387efc9aa4ae4c56b06
jakob <jakob.dhondt@student.kuleuven.be>
2014-03-09 21:48:06 +0100

    alfkjsdf

--- src/initLine/Get.java
+++ src/initLine/Get.java
@@ -19,23 +19,6 @@ public class Get extends Head {
 	public Get(String[] clientSentence, DataOutputStream outToClient){
 		super(clientSentence, outToClient);
 	}
-	
-	
-	public int[] getData(){
-		int[] data;
-		try{
-			String fileName = clientSentence[1];
-			if(fileName.contains("/"))
-				fileName = fileName.substring(1);
-			data = readFile(fileName);
-		}catch(Exception e){
-			data = new int[3];
-			data[0] = 4;
-			data[1] = 0;
-			data[2] = 4;
-		}
-		return data;
-	}
 
 	/**
 	 * This method returns a string with the information expected from a get-command.
@@ -62,42 +45,31 @@ public class Get extends Head {
 		 return output;
 	}
 	
-	
-	public int[] readFile(String fileName)
-			throws FileNotFoundException, IOException {
-		int[] data;
-		//option 1 (not good)
-//		output = FileUtils.readFileToString(new File(fileName));
-		//option 2 (not good)
-//		BufferedReader br = new BufferedReader(new FileReader(fileName));
-//		 String line = null;
-//		 while ((line = br.readLine()) != null) {
-//		   output += line + "\n";
-//		 }
-		//option 3 (crap)
-//		BufferedReader br = new BufferedReader(new FileReader(fileName));
-//		 int line = 0;
-//		 while ((line = br.read()) != -1) {
-//		   output += line;
-//		 }
-		//option 4
-		FileInputStream fis = new FileInputStream(new File(fileName));
-		int size = fis.available();
-		data = new int[size];
-		for(int i = 0; i < size; i++) {
-			   data[i] = fis.read();
-			 }
-		fis.close();
-		return data;
-	}
-	
-	@Override
-	public void execute() throws IOException{
-		outToClient.writeBytes(getResponse());
-//		int[] data = getData();
-//		outToClient.writeBytes(super.getResponse());
-//		for(int i : data){
-//			outToClient.writeByte(i);
+//	private void readFileToOutput() throws IOException{
+//		try{
+//			String fileName = clientSentence[1];
+//			if(fileName.contains("/"))
+//				fileName = fileName.substring(1);
+//			File f = new File(fileName);
+//			InputStream in = new FileInputStream(f);
+//			int length = (int) f.length();
+//			int read = 0;
+//			byte[] bytes = new byte[length];
+//			while ((read = in.read(bytes)) != -1 && length > 0) {
+//				length = length-read; //Prevent other requests on inputstream to be read
+//				outToClient.write(bytes, 0, read);
+//			}
+//			in.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			String error = "\r\n\r\n404 Not Found";
+//			outToClient.writeBytes(error);
 //		}
-	}
+//	}
+//	
+//	@Override
+//	public void execute() throws IOException{
+//		super.execute();
+//		readFileToOutput();
+//	}
 }
