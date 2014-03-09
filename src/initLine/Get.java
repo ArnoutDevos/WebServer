e0e292e78cc1041e9a5286c2df63c4d8eb3b2c76
jakob <jakob.dhondt@student.kuleuven.be>
2014-03-09 11:01:35 +0100

    probleem van de foto proberen op te lossen maar niet gelukt

--- src/initLine/Get.java
+++ src/initLine/Get.java
@@ -2,6 +2,8 @@ package initLine;
 
 import java.io.*;
 
+import org.apache.commons.io.FileUtils;
+
 public class Get extends Head {
 	
 	
@@ -9,21 +11,56 @@ public class Get extends Head {
 		super(clientSentence, outToClient);
 	}
 	
-	@Override
-	public String getResponse(){
-		String output = super.getResponse() + "\r\n";
+	public int[] getData(){
+		int[] data;
 		try{
 			String fileName = clientSentence[1];
 			if(fileName.contains("/"))
 				fileName = fileName.substring(1);
-			BufferedReader br = new BufferedReader(new FileReader(fileName));
-			 String line = null;
-			 while ((line = br.readLine()) != null) {
-			   output += line + "\n";
-			 }
+			data = readFile(fileName);
 		}catch(Exception e){
-			output += "404 Not Found";
+			data = new int[3];
+			data[0] = 4;
+			data[1] = 0;
+			data[2] = 4;
 		}
-		return output + "\n";
+		return data;
+	}
+
+	public int[] readFile(String fileName)
+			throws FileNotFoundException, IOException {
+		int[] data;
+		//option 1 (not good)
+//		output = FileUtils.readFileToString(new File(fileName));
+		//option 2 (not good)
+//		BufferedReader br = new BufferedReader(new FileReader(fileName));
+//		 String line = null;
+//		 while ((line = br.readLine()) != null) {
+//		   output += line + "\n";
+//		 }
+		//option 3 (crap)
+//		BufferedReader br = new BufferedReader(new FileReader(fileName));
+//		 int line = 0;
+//		 while ((line = br.read()) != -1) {
+//		   output += line;
+//		 }
+		//option 4
+		FileInputStream fis = new FileInputStream(new File(fileName));
+		int size = fis.available();
+		data = new int[size];
+		for(int i = 0; i < size; i++) {
+			   data[i] = fis.read();
+			 }
+		fis.close();
+		return data;
+	}
+	
+	@Override
+	public void execute() throws IOException{
+		int[] data = getData();
+		outToClient.writeBytes(super.getResponse());
+		for(int i : data){
+			outToClient.writeByte(i);
+		}
 	}
 }
